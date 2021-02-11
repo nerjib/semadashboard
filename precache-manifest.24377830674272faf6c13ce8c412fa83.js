c5dd298526f59b8de2e213c643db7622c5ccaaff
nerjib <kabirnajib0@gmail.com>
2021-02-11 10:50:43 +0100

    Updates

---
+++ precache-manifest.24377830674272faf6c13ce8c412fa83.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76c38a56ba49c38e809b68ea89b800c",
+    "url": "/semadashboard/index.html"
+  },
+  {
+    "revision": "cd792d2990e478e24e6f",
+    "url": "/semadashboard/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "2062f7afaee987bb0448",
+    "url": "/semadashboard/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "cd792d2990e478e24e6f",
+    "url": "/semadashboard/static/js/2.c3e05165.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/semadashboard/static/js/2.c3e05165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2062f7afaee987bb0448",
+    "url": "/semadashboard/static/js/main.a1b00454.chunk.js"
+  },
+  {
+    "revision": "63dd7d971ad561ffbd8e",
+    "url": "/semadashboard/static/js/runtime-main.e9122acb.js"
+  },
+  {
+    "revision": "636c3af61771f8f62f703413ec253a68",
+    "url": "/semadashboard/static/media/kaduna-map.636c3af6.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/semadashboard/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "e6ee3c5b800b8f39c2452a45170f7b7e",
+    "url": "/semadashboard/static/media/semaLogo.e6ee3c5b.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/semadashboard/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/semadashboard/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/semadashboard/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/semadashboard/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
